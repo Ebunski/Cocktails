bea76ee74fa34a068f254bf306ec5e432f545522
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-07-24 12:07:05 +0100

    base  routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
-  )
-}
+    <header className="navbar">
+      <nav className="nav-center">
+        <img className="logo" src={logo} alt="the cocktail db" />
+        <ul className="nav-links">
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">about</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
