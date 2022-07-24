6b656fa0435d81c903c9954c0b892fd80b6c3b4c
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-07-24 12:24:21 +0100

    about route

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in
+        molestiae ipsa magnam, dolorem totam voluptatum! Nulla eos, corporis
+        fugit, maxime architecto recusandae placeat explicabo sunt eligendi
+        animi dolore debitis?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
